602f4a44d007659ba64ed490364ec40e47c1c1d1
qianxiaofei <qianxiaofei@lashou-inc.com>
2016-08-30 16:50:59 +0800

    semaphore

---
+++ src/main/java/com/thread/concurrent/SemaphoreExample.java
@@ -0,0 +1,43 @@
+package com.thread.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 
+ *  A counting semaphore.  Conceptually, a semaphore maintains a set of permits
+ * <code>Semaphore</code>
+ */
+public class SemaphoreExample {
+
+    public static void main(String[] args) {
+	 // 线程池
+        ExecutorService exec = Executors.newCachedThreadPool();
+        // 只能5个线程同时访问
+        final Semaphore semp = new Semaphore(5);
+        // 模拟50个客户端访问
+        for (int index = 0; index < 50; index++) {
+            final int NO = index;
+            Runnable run = new Runnable() {
+                public void run() {
+                    try {
+                        // 获取许可
+                        semp.acquire();
+                        System.out.println("Accessing: " + NO);
+                        Thread.sleep((long) (Math.random() * 6000));
+                        // 访问完后，释放
+                        semp.release();
+                        //availablePermits()指的是当前信号灯库中有多少个可以被使用
+                        System.out.println("-----------------" + semp.availablePermits()); 
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            exec.execute(run);
+        }
+        // 退出线程池
+        exec.shutdown();
+    }
+}
